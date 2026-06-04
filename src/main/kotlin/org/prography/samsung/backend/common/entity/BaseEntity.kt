dfe0203f494518afdf001b21430b28eacc748f5b
Jeyong <dlwpdyd201@naver.com>
2026-06-04 23:29:04 +0900

    refactor: BaseEntity 필드를 LocalDateTime -> Instant로 변경

--- src/main/kotlin/org/prography/samsung/backend/common/entity/BaseEntity.kt
+++ src/main/kotlin/org/prography/samsung/backend/common/entity/BaseEntity.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.MappedSuperclass
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.time.LocalDateTime
+import java.time.Instant
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
@@ -14,9 +14,9 @@ abstract class BaseEntity {
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
-    val createdAt: LocalDateTime? = null
+    val createdAt: Instant? = null
 
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
-    val updatedAt: LocalDateTime? = null
+    val updatedAt: Instant? = null
 }
