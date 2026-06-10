e9f790a7b88fc7a6117248a2c926619c8260db54
Jeyong <dlwpdyd201@naver.com>
2026-06-10 20:06:08 +0900

    chore: 코드 스타일 개선 (import 제거)

--- src/main/kotlin/org/prography/samsung/backend/common/auth/CurrentUserHolder.kt
+++ src/main/kotlin/org/prography/samsung/backend/common/auth/CurrentUserHolder.kt
@@ -1,11 +1,14 @@
 package org.prography.samsung.backend.common.auth
 
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.RequestContextHolder
+
 object CurrentUserHolder {
     const val REQUEST_ATTRIBUTE = "currentUser"
 
     fun get(): CurrentUser = (
-        org.springframework.web.context.request.RequestContextHolder.getRequestAttributes()
-            ?.getAttribute(REQUEST_ATTRIBUTE, org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST)
+        RequestContextHolder.getRequestAttributes()
+            ?.getAttribute(REQUEST_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST)
             as? CurrentUser
         )
         ?: throw IllegalStateException("Current user is not available")
